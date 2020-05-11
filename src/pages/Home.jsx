70ecf2e569887e8ab4d2a5e2b5ddb4636584b936
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-11 08:28:47 +0300

    Created Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { Header } from "../components/Header";
 
 export const Home = () => {
-  return <h1>Home page</h1>;
+  return (
+    <Header title="Amazing recipes">
+      <Link
+        to="/recipes"
+        className="text-uppercase btn btn-secondary btn-lg mt-3"
+      >
+        Search recipes
+      </Link>
+    </Header>
+  );
 };
