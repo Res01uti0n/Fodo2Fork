66edfe90e5be12a16c9b3001f83c28c31220a33d
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-11 08:38:15 +0300

    Created default page

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { Header } from "../components/Header";
 
 export const Default = () => {
-  return <h1>Error page</h1>;
+  return (
+    <Header title="404" styleClass="default-hero">
+      <h2 className="text-light text-uppercase">You`re in the wrong place</h2>
+
+      <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+        Return home
+      </Link>
+    </Header>
+  );
 };
