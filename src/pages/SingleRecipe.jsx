5239fa25dc58ffd1b573eb1de9fdad68463c7b3e
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-12 08:19:46 +0300

    Created SingleRecipe component

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const SingleRecipe = () => {
-  return <h1>Single Recipe page</h1>;
+import { recipeData } from "../data/tempDetails";
+import { Link } from "react-router-dom";
+
+export const SingleRecipe = ({ match }) => {
+  const [values, setValues] = useState({
+    recipe: recipeData,
+    id: match.params.id,
+    loading: false,
+  });
+
+  if (values.loading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <h2 className="text-uppercase text-orange text-center">
+              Loading recipe ...
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-6 my-3">
+          <Link to="/recipes" className="btn btn-warning text-capitalize mb-5">
+            Back to recipes list
+          </Link>
+
+          <img
+            src={values.recipe.image_url}
+            className="d-block w-100"
+            style={{ maxHeight: "30rem" }}
+            alt="recipe"
+          />
+        </div>
+
+        <div className="col-10 mx-auto col-md-6 my-3">
+          <h6 className="text-uppercase">{values.recipe.title}</h6>
+
+          <h6 className="text-capitalize text-slanted text-warning">
+            Provided by {values.recipe.publisher}
+          </h6>
+
+          <a
+            href={values.recipe.publisher_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary mt-2 text-capitalize"
+          >
+            Recipe url
+          </a>
+
+          <ul className="list-group mt-4">
+            <h2 className="mt-3 mb-4">Ingredients</h2>
+
+            {values.recipe.ingredients.map((item, index) => (
+              <li key={index} className="list-group-item text-slanted">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
